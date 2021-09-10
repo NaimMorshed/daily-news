6820583930265e6028f9eac081fa2a4236c06d36
Naim Morshed <76660049+NaimMorshed@users.noreply.github.com>
2021-09-10 21:12:24 +0600

    Update NavBar.js

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -1,20 +1,34 @@
 import React from 'react';
 
+const Classes = {
+    root: "bg-white flex justify-between p-5",
+    logoStyle: { fontFamily: "'Abril Fatface', cursive", },
+    logo: "font-bold text-3xl",
+    daily: "text-red-600 tracking-tighter",
+    news: "tracking-widest",
+    navItems: "mx-3 text-xl",
+    buttons: "mx-2 bg-blue-500 rounded text-white px-3",
+}
+
 const NavBar = () => {
     return (
-        <div className="bg-white flex justify-between p-5">
+        <div className={Classes.root}>
             <section>
-                <h3>LOGO</h3>
+                <span style={Classes.logoStyle} className={Classes.logo}>
+                    <span className={Classes.daily}>DAILY</span>
+                    <span className={Classes.news}>NEWS</span>
+                </span>
             </section>
             <section>
-                <span className="mx-3">Home</span>
-                <span className="mx-3">Home</span>
-                <span className="mx-3">Home</span>
-                <span className="mx-3">Home</span>
+                <span className={Classes.navItems}>Home</span>
+                <span className={Classes.navItems}>World</span>
+                <span className={Classes.navItems}>Technology</span>
+                <span className={Classes.navItems}>Sport</span>
+                <span className={Classes.navItems}>Entertainment</span>
             </section>
             <section>
-                <button className="mx-2 bg-blue-500 rounded text-white px-3">Login</button>
-                <button className="mx-2 bg-blue-500 rounded text-white px-3">Logout</button>
+                <button className={Classes.buttons}>Login</button>
+                <button className={Classes.buttons}>Logout</button>
             </section>
         </div>
     );
